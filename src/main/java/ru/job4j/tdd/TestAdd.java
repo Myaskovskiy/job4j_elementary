529fa6f6c60e521ca7790ff549578711db2dc6ba
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-18 16:26:44 +0300

    java doc new

--- src/main/java/ru/job4j/tdd/TestAdd.java
+++ src/main/java/ru/job4j/tdd/TestAdd.java
@@ -9,6 +9,7 @@ public class TestAdd {
      * Метод принимает на вход два числа и возвращает сумму.
      * @param v1 первое число
      * @param v2 второе число
+     * @return возвращает сумму чисел
      */
     public int add(int v1, int v2) {
         return v1 + v2;
