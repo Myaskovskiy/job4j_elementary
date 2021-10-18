b3b48cce0372e40dfc82a2f2ddb10f9e37cf33c0
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-18 13:37:43 +0300

    test JaCoCo new

---
+++ src/main/java/ru/job4j/tdd/TestAdd.java
@@ -0,0 +1,7 @@
+package ru.job4j.tdd;
+
+public class TestAdd {
+    public int add(int v1, int v2) {
+        return v1 + v2;
+    }
+}
--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -1,14 +1,17 @@
 package ru.job4j;
 
 import org.junit.Test;
+import ru.job4j.tdd.TestAdd;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainTest {
     @Test
     public void whenActionThenResult() {
-        int excepted = 1;
-        int result = 1;
+        TestAdd testAdd = new TestAdd();
+        int excepted = testAdd.add(1, 1);
+        int result = 2;
         assertThat(result, is(excepted));
     }
 }
