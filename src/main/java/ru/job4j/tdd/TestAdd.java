374e82696c3aa4242fa9548bcf890a68b550af6e
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-18 13:48:31 +0300

    java doc

--- src/main/java/ru/job4j/tdd/TestAdd.java
+++ src/main/java/ru/job4j/tdd/TestAdd.java
@@ -1,6 +1,15 @@
 package ru.job4j.tdd;
-
+/**
+ * Класс описывает работу метода сложения
+ * @author V Myaskovskiy
+ * @version 1.0
+ */
 public class TestAdd {
+    /**
+     * Метод принимает на вход два числа и возвращает сумму.
+     * @param v1 первое число
+     * @param v2 второе число
+     */
     public int add(int v1, int v2) {
         return v1 + v2;
     }
