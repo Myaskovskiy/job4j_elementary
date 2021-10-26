0961aaafb4203f577c747d3110bf12d54dab91c6
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-26 11:39:35 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/lambda/Test.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+import java.util.function.Predicate;
+
+public class Test {
+
+        public boolean empty(String s) {
+        return s.isEmpty();
+        }
+
+        public boolean startWith(String s, String pref) {
+        return s.startsWith(pref);
+        }
+
+        public boolean contains(String s, String key) {
+        return s.contains(key);
+        }
+
+        public boolean check(Predicate<String> pr, String s) {
+            return pr.test(s);
+        }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.empty(""));
+        System.out.println(test.contains("1234", "12"));
+        System.out.println(test.startWith("12345", "1"));
+        System.out.println("test");
+        System.out.println(test.check(str -> str.contains("12"), "12345"));
+        System.out.println(test.check(str -> str.isEmpty(), ""));
+    }
+}
