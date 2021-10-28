2574669ed054fcf9d595452e412dbe25a4b547a2
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-28 11:09:17 +0300

    2.1. Лямбда [#58165 #220799] new

--- src/main/java/ru/job4j/lambda/Test.java
+++ src/main/java/ru/job4j/lambda/Test.java
@@ -2,6 +2,7 @@ package ru.job4j.lambda;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Test {
@@ -24,12 +25,18 @@ public class Test {
 
     public static void main(String[] args) {
         Test test = new Test();
+        List<String> strings = Arrays.asList("111", "2222", "4", "33333");
+        Comparator<String> cmpText = (o1, o2) -> o1.compareTo(o2);
+        Comparator<String> cmpDescSize = (o1, o2) -> Integer.compare(o2.length(), o1.length());
+        strings.sort(cmpText);
+        System.out.println(strings);
+        strings.sort(cmpDescSize);
+        System.out.println(strings);
         System.out.println(test.empty(""));
         System.out.println(test.contains("1234", "12"));
         System.out.println(test.startWith("12345", "1"));
         System.out.println("test");
         System.out.println(test.check(str -> str.contains("12"), "12345"));
         System.out.println(test.check(str -> str.isEmpty(), ""));
-
     }
 }
