e503183710377b7e4159097a7d93bcff7592198e
vmyaskovskiy <slavav1977@yandex.ru>
2021-10-28 11:31:57 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,23 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("111", "2222", "4", "33333");
+        Comparator<String> cmpText = (o1, o2) -> {
+            System.out.println("compare text - " + o1 + " and " + o2);
+            return o1.compareTo(o2);
+        };
+        Comparator<String> cmpDescSize = (o1, o2) -> {
+            System.out.println("compare size - " + o1 + " and " + o2);
+            return Integer.compare(o2.length(), o1.length());
+        };
+        strings.sort(cmpText);
+        System.out.println(strings);
+        strings.sort(cmpDescSize);
+        System.out.println(strings);
+    }
+}
