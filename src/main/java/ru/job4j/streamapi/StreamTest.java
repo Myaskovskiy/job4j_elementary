9fadf617275adf8abdd0da3ed1d1b12a2de61291
vmyaskovskiy <slavav1977@yandex.ru>
2021-11-11 15:54:51 +0300

    0. Stream API [#58166 #227256]

--- src/main/java/ru/job4j/streamapi/StreamTest.java
+++ src/main/java/ru/job4j/streamapi/StreamTest.java
@@ -8,9 +8,9 @@ import java.util.stream.Collectors;
 public class StreamTest {
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, -3, 4));
         List<Integer> res = arrayList.stream().filter(
-                x -> x % 2 == 0
+                x -> x > 0
         ).collect(Collectors.toList());
         res.forEach(System.out::println);
     }
