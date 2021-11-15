a5bf568e44488e8b4cfbd9bb49effc2c03cbe4d4
vmyaskovskiy <slavav1977@yandex.ru>
2021-11-15 12:42:32 +0300

    2. Список адресов [#110062 #227683]new

--- src/main/java/ru/job4j/streamapi/Profiles.java
+++ src/main/java/ru/job4j/streamapi/Profiles.java
@@ -1,12 +1,10 @@
 package ru.job4j.streamapi;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    /*
-    //test
-     */
 
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
@@ -16,7 +14,7 @@ public class Profiles {
 
     public List<Address> sorted(List<Profile> profiles) {
         return collect(profiles).stream().
-                sorted(((o1, o2) -> o1.getCity().compareTo(o2.getCity())))
+                sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
