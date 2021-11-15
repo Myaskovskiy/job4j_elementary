482255d8ee7256abe05070c50685e4602668b33b
vmyaskovskiy <slavav1977@yandex.ru>
2021-11-15 10:30:03 +0300

    2. Список адресов [#110062 #227683]

--- src/main/java/ru/job4j/streamapi/Profiles.java
+++ src/main/java/ru/job4j/streamapi/Profiles.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().
-                 map(Profile::getAddress).
-                collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 
     public List<Address> sorted(List<Profile> profiles) {
