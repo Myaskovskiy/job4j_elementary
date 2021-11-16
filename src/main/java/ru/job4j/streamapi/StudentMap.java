055e4a45e22d721b4d1677aff0a1e97912d14130
vmyaskovskiy <slavav1977@yandex.ru>
2021-11-16 10:00:06 +0300

    4. Преобразование List в Map. [#110226 #227737]new

--- src/main/java/ru/job4j/streamapi/StudentMap.java
+++ src/main/java/ru/job4j/streamapi/StudentMap.java
@@ -3,7 +3,9 @@ package ru.job4j.streamapi;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+/*
+//test
+ */
 public class StudentMap {
     public Map<String, Student> studentMap(List<Student> students) {
         return students.stream()
