cfdcc2ed0acf61c66660e27c0b9ef4d08d878773
vmyaskovskiy <slavav1977@yandex.ru>
2021-11-18 11:08:04 +0300

    8. Перенос методов в Stream API [#279131 #229517]

--- src/main/java/ru/job4j/streamapi/Card.java
+++ src/main/java/ru/job4j/streamapi/Card.java
@@ -1,10 +1,37 @@
 package ru.job4j.streamapi;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 public class Card {
 
     private static Suit suit;
     private static Value value;
+    Set<CardString> cards = new HashSet<>();
+
+    static class CardString {
+        String suit;
+        String value;
+
+        public CardString(String suit, String value) {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "CardString{"
+                    + "suit='"
+                    + suit
+                    + '\''
+                    + ", value='"
+                    + value
+                    + '\''
+                    + '}';
+        }
+    }
 
     public Card(Suit suit, Value value) {
         this.suit = suit;
@@ -16,10 +43,19 @@ public class Card {
                 .flatMap(su -> Stream.of(Value.values())
                         .map(val -> su + " " + val))
                 .forEach(System.out::println);
+
+        this.cards = Stream.of(Suit.values())
+                .flatMap(su -> Stream.of(Value.values())
+                        .map(val -> new CardString(su.toString(), val.toString())))
+                .collect(Collectors.toSet());
+
+        cards.stream().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
+
         Card card = new Card(suit, value);
         card.sort();
+
     }
 }
