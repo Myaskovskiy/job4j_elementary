be4617dbc29994e46254b7ff0dc6e1a958a78ba7
vmyaskovskiy <slavav1977@yandex.ru>
2021-11-22 11:20:03 +0300

    8. Перенос методов в Stream API [#279131 #229517]new

--- src/main/java/ru/job4j/streamapi/Card.java
+++ src/main/java/ru/job4j/streamapi/Card.java
@@ -1,5 +1,6 @@
 package ru.job4j.streamapi;
 
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,15 +14,50 @@ public class Card {
         this.value = value;
     }
 
-    public void sort() {
-         Stream.of(Suit.values())
+    @Override
+    public String toString() {
+        return "card{"
+                + "suit='" + suit + '\''
+                + ", value=" + value
+                + '}';
+    }
+
+    public class CardString {
+         String suit;
+         String value;
+
+        public CardString(String suit, String value) {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "CardString{" + "suit='"
+                    + suit + '\'' + ", value='"
+                    + value + '\'' + '}';
+        }
+    }
+
+    public Set<Card> sort() {
+       return  Stream.of(Suit.values())
                 .flatMap(su -> Stream.of(Value.values())
                         .map(val -> new Card(su, val)))
-                 .collect(Collectors.toSet()).forEach(System.out::println);
+                 .collect(Collectors.toSet());
+    }
+
+    public Set<CardString> sortNew() {
+        return  Stream.of(Suit.values())
+                .flatMap(su -> Stream.of(Value.values())
+                        .map(val -> new CardString(su.toString(), val.toString())))
+                .collect(Collectors.toSet());
     }
 
     public static void main(String[] args) {
         Card card = new Card(suit, value);
-        card.sort();
+        Set<Card> set = card.sort();
+        Set<CardString> set1 = card.sortNew();
+        System.out.println(set);
+        System.out.println(set1);
     }
 }
